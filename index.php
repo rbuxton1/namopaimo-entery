4bc72701038f88f6751490460b883d077d964579
Ryan Buxton <rbuxton1@gmail.com>
2017-12-29 14:57:57 -0700

    Reformating

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <div class="w3-center">
       <div class="w3-card-4">
         <div class="w3-container w3-green">
-          <h2>Submit a model</h2>
+          <h2>2018 Registration</h2>
         </div>
         <form action="" method="post" class="w3-container">
           <div class='w3-row-padding'> <!-- First name -->
@@ -40,14 +40,24 @@
               <input class="w3-input" type="text" name="last" placeholder="Doe" required>
             </div>
           </div>
-          <div class='w3-row-padding'> <!-- Address -->
+          <div class='w3-row-padding'> <!-- Address Line 1-->
             <div class='w3-quarter'>
               <p>
-                Address (This will be used if you win any prizes, optional if you dont want any)
+                Address
               </p>
             </div>
             <div class='w3-threequarter'>
-              <input class="w3-input" type="text" name="first" placeholder="123 S Horse St" required>
+              <input class="w3-input" type="text" name="address" placeholder="123 S Horse St">
+            </div>
+          </div>
+          <div class='w3-row-padding'> <!-- Address Line 2 (Country)-->
+            <div class='w3-quarter'>
+              <p>
+                Country
+              </p>
+            </div>
+            <div class='w3-threequarter'>
+              <input class="w3-input" type="text" name="country" placeholder="USA" required>
             </div>
           </div>
           <div class='w3-row-padding'> <!-- Experience Level -->
@@ -57,11 +67,13 @@
               </p>
             </div>
             <div class='w3-threequarter'>
-              <select class="w3-select" name="option" required>
+              <select class="w3-select" name="level" required>
                 <option value="" disabled selected>Choose your option</option>
-                <option value="1">Beginner</option>
-                <option value="2">Intermediate</option>
-                <option value="3">Expert</option>
+                <option value="1">First Time Painter</option>
+                <option value="2">Beginner</option>
+                <option value="3">Intermediate</option>
+                <option value="4">Advanced Amatuer</option>
+                <option value="5">Professional</option>
               </select>
             </div>
           </div>
@@ -72,9 +84,7 @@
               </p>
             </div>
             <div class='w3-threequarter'>
-
-
-              <textarea name="desc" rows="2" class="w3-input" placeholder="Whats so cool about your horse?" required></textarea>
+              <textarea name="desc" rows="2" class="w3-input" placeholder="Make / Mold / Scale" required></textarea>
             </div>
           </div>
           <div class='w3-row-padding'> <!-- Painting plan -->
@@ -84,11 +94,30 @@
               </p>
             </div>
             <div class='w3-threequarter'>
-              <textarea name="plan" rows="2" class="w3-input" placeholder="How do you plan on doing this?"></textarea>
+              <textarea name="plan" rows="3" class="w3-input" placeholder="Color / Medium"></textarea>
+            </div>
+          </div>
+          <div class='w3-row-padding'> <!-- Goals / Challenges -->
+            <div class='w3-quarter'>
+              <p>
+                Personal Goals or challenges
+              </p>
+            </div>
+            <div class='w3-threequarter'>
+              <textarea name="data" rows="3" class="w3-input" placeholder="(optional)"></textarea>
+            </div>
+          </div>
+          <div class='w3-row-padding'> <!-- entry fee payed?-->
+            <div class='w3-quarter'>
+              <p>
+                Entry fee paid? <i>($2 entry fee, optional but very much appreciated. PayPal: NaMoPaiMo@gmail.com)</i>
+              </p>
+            </div>
+            <div class='w3-threequarter'>
+              <input class="w3-check" type="checkbox" name="paid">
             </div>
           </div>
 
-          <p> All submissions have to pay an entery fee. This fee is to be payed to $pay, and is of $ammount. By clicking submit you are awknowledging this and are aware that failure to pay that fee will result in your submission not being counted. You will also not be able to change anything about this once it is submitted so be sure that your data above is correct!</p>
           <input type="submit" class="w3-btn w3-dark-gray" value="Submit" name="submit">
         </form>
     </div>
