c0bfc3d4c6f98a69c177bd92cb32042289f04eb9
Ryan Buxton <rbuxton1@gmail.com>
2017-12-30 18:36:26 -0700

    Just redirect to the thing

---
+++ complete.php
@@ -0,0 +1,45 @@
+<html>
+  <title>NaMoPaiMo</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+  <body>
+    <!-- Header -->
+    <header class="w3-container w3-theme w3-padding" id="myHeader">
+      <div class="w3-center">
+        <h4>National Model Painting Month</h4>
+        <h1 class="w3-xxxlarge w3-animate-bottom">NaMoPaiMo</h1>
+      </div>
+    </header>
+
+    <br>
+
+    <!-- Body -->
+    <div class="w3-center">
+      <div class="w3-card-4">
+        <div class="w3-container w3-green">
+          <h3>Registration Complete!</h3>
+        </div>
+        <p>
+          Thank you for registering for NaMoPaiMo 2018!
+        </p>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="w3-container w3-theme-dark w3-padding-16">
+      <h3>NaMoPaiMo</h3>
+      <p>
+        Ryan Buxton 2017<br>
+        <i><a href="http://www.github.com/rbuxton1/namopaimo-entry">Heres how it works</a></i>
+      </p>
+      <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
+        <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
+        <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
+        <i class="fa fa-chevron-circle-up"></i></span></a>
+      </div>
+    </footer>
+  </body>
+</html>
