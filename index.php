9f67d71d19ad5cbab9c92bd98150e8f8e5b8709a
Ryan Buxton <rbuxton1@gmail.com>
2018-01-01 20:20:53 -0700

    Auto indented plus made it not inside

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <html>
-  <title>NaMoPaiMo</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
-  <body>
-    <!-- Header -->
-    <header class="w3-container w3-theme w3-padding" id="myHeader">
-      <div class="w3-center">
-        <h4>National Model Painting Month</h4>
-        <h1 class="w3-xxxlarge w3-animate-bottom">NaMoPaiMo</h1>
-      </div>
-    </header>
-
-    <br>
-
-    <!-- Body -->
+<title>NaMoPaiMo</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<body>
+  <!-- Header -->
+  <header class="w3-container w3-theme w3-padding" id="myHeader">
     <div class="w3-center">
-      <div class="w3-card-4">
-        <div class="w3-container w3-green">
-          <h2>2018 Registration</h2>
-        </div>
-        <form action="" method="post" class="w3-container">
-          <div class='w3-row-padding'> <!-- First name -->
-            <div class='w3-quarter'>
-              <p>First Name</p>
-            </div>
-            <div class='w3-threequarter'>
-              <input class="w3-input" type="text" name="first" placeholder="Darcy" maxlength="20" required>
+      <h4>National Model Painting Month</h4>
+      <h1 class="w3-xxxlarge w3-animate-bottom">NaMoPaiMo</h1>
+    </div>
+  </header>
+
+  <br>
+
+  <!-- Body -->
+  <div class="w3-center">
+    <div class="w3-card-4">
+      <div class="w3-container w3-green">
+        <h2>2018 Registration</h2>
+      </div>
+      <form action="" method="post" class="w3-container">
+        <div class='w3-row-padding'> <!-- First name -->
+          <div class='w3-quarter'>
+            <p>First Name</p>
+          </div>
+          <div class='w3-threequarter'>
+            <input class="w3-input" type="text" name="first" placeholder="Darcy" maxlength="20" required>
           </div>
           <div class='w3-row-padding'> <!-- Last Name -->
             <div class='w3-quarter '>
@@ -149,57 +149,57 @@
           <input type="submit" class="w3-btn w3-dark-gray" value="Submit" name="submit">
         </form>
       </div>
-      <br>
-
-      <?php
-        include("../../config.php");
-
-        if(isset($_POST["submit"])) {
-          $first = mysqli_real_escape_string($db, $_POST['first']);
-          $last = mysqli_real_escape_string($db, $_POST['last']);
-          $email = mysqli_real_escape_string($db, $_POST['email']);
-          $address = mysqli_real_escape_string($db, $_POST['address']) . ", " . mysqli_real_escape_string($db, $_POST['address2']) . ", " .mysqli_real_escape_string($db, $_POST['city']) . ", " .mysqli_real_escape_string($db, $_POST['state']) . ", " . mysqli_real_escape_string($db, $_POST['zip']);
-          $country = mysqli_real_escape_string($db, $_POST['country']);
-          $level = mysqli_real_escape_string($db, $_POST['level']);
-          $desc = mysqli_real_escape_string($db, $_POST['desc']);
-          $medium = mysqli_real_escape_string($db, $_POST['medium']);
-          $color = mysqli_real_escape_string($db, $_POST['color']);
-          $data = mysqli_real_escape_string($db, $_POST['data']);
-          $fee = mysqli_real_escape_string($db, $_POST['fee']);
-
-          $stmt = "INSERT INTO `user`(`id`, `first`, `last`, `email`, `address`, `country`, `level`, `desc`, `mediums`, `color`, `extra`, `paid`) VALUES (NULL, '$first', '$last', '$email', '$address', '$country', '$level', '$desc', '$medium', '$color', '$data', '$fee')";
-          $result = mysqli_query($db,$stmt);
-
-          if($result){
-            echo "
-              <br>
-              <div class='w3-card-4'>
-                <div class='w3-container w3-green'>
-                  <h2>Registration complete!</h2>
-                </div>
-                <p>
-                  Good luck!
-                </p>
-              </div>
-              ";
-          }
-        }
-      ?>
     </div>
-    <br>
+  </div>
+  <?php
+    include("../../config.php");
 
-    <!-- Footer -->
-    <footer class="w3-container w3-theme-dark w3-padding-16">
-      <h3>NaMoPaiMo</h3>
-      <p>
-        Ryan Buxton 2017<br>
-        <i><a href="http://www.github.com/rbuxton1/namopaimo-entry">Heres how it works</a></i>
-      </p>
-      <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
-        <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
-        <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
+    if(isset($_POST["submit"])) {
+      $first = mysqli_real_escape_string($db, $_POST['first']);
+      $last = mysqli_real_escape_string($db, $_POST['last']);
+      $email = mysqli_real_escape_string($db, $_POST['email']);
+      $address = mysqli_real_escape_string($db, $_POST['address']) . ", " . mysqli_real_escape_string($db, $_POST['address2']) . ", " .mysqli_real_escape_string($db, $_POST['city']) . ", " .mysqli_real_escape_string($db, $_POST['state']) . ", " . mysqli_real_escape_string($db, $_POST['zip']);
+      $country = mysqli_real_escape_string($db, $_POST['country']);
+      $level = mysqli_real_escape_string($db, $_POST['level']);
+      $desc = mysqli_real_escape_string($db, $_POST['desc']);
+      $medium = mysqli_real_escape_string($db, $_POST['medium']);
+      $color = mysqli_real_escape_string($db, $_POST['color']);
+      $data = mysqli_real_escape_string($db, $_POST['data']);
+      $fee = mysqli_real_escape_string($db, $_POST['fee']);
+
+      $stmt = "INSERT INTO `user`(`id`, `first`, `last`, `email`, `address`, `country`, `level`, `desc`, `mediums`, `color`, `extra`, `paid`) VALUES (NULL, '$first', '$last', '$email', '$address', '$country', '$level', '$desc', '$medium', '$color', '$data', '$fee')";
+      $result = mysqli_query($db,$stmt);
+
+      if($result){
+        echo "
+        <br>
+        <div class='w3-card-4 w3-center'>
+          <div class='w3-container w3-green'>
+            <h2>Registration complete!</h2>
+          </div>
+          <p>
+            Good luck!
+          </p>
+        </div>
+      ";
+      unset($_POST['submit']);
+    }
+  }
+  ?>
+  <br>
+
+  <!-- Footer -->
+  <footer class="w3-container w3-theme-dark w3-padding-16">
+    <h3>NaMoPaiMo</h3>
+    <p>
+      Ryan Buxton 2017<br>
+      <i><a href="http://www.github.com/rbuxton1/namopaimo-entry">Heres how it works</a></i>
+    </p>
+    <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
+      <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
+      <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
         <i class="fa fa-chevron-circle-up"></i></span></a>
       </div>
     </footer>
   </body>
-</html>
+  </html>
